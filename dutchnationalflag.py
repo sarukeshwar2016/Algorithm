b782ada8ac95b5e01548be346357e66179fe404f
Sarukeshwar <sarukeshwar2016@gmail.com>
2025-11-13 14:00:39 +0530

    style: enforce single quotes in dutchnationalflag.py

--- dutchnationalflag.py
+++ dutchnationalflag.py
@@ -1,8 +1,8 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
+        '"
         Do not return anything, modify nums in-place instead.
-        """
+        '"
         low,mid,high=0,0,len(nums)-1
         while mid<=high:
             if nums[mid]==0:
